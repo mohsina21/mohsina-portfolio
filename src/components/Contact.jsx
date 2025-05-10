63e3c44d6c33810f7c66aff79a137bf539a3fa64
Mohsina <mohsina734parveen@gmail.com>
2025-05-11 05:11:03 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
       
       <div className="flex flex-col items-center">
         <a
-          href="/Resume_Mohsina.pdf.pdf"
+          href="/Resume_MohsinaParveen.pdf"
           download
           className="bg-pink-500 text-white px-6 py-3 rounded-lg hover:bg-pink-600 transition text-lg font-semibold"
         >
